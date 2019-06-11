cd06b36378deeb3f7c619c660a258618eed07d5e
Vikasvics <vikasvics19@gmail.com>
2019-06-11 16:03:53 +0530

    Task2 commit

---
+++ src/Task2/Classcomponent1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Classcomponent1 extends Component {
+    render() {
+        return (
+            <div>
+                Name:
+            </div>
+        );
+    }
+}
+
+export default Classcomponent1;
